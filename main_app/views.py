5aac688ba2de1b309266cfc72f9b75618173a3b0
jeremiahtaylor2021 <jeremiahtaylor2021@gmail.com>
2022-10-01 16:40:56 -0700

    wip: auth with built in user model

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,7 @@
+from django.shortcuts import redirect
 from django.views.generic import TemplateView, CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
@@ -6,6 +9,11 @@ class Home(TemplateView):
     template_name = 'main_app/home.html'
 
 class Signup(CreateView):
-    form_class = UserCreationForm
-    success_url = '/'
     template_name = 'registration/signup.html'
+    success_url = reverse_lazy('home')
+    form_class = UserCreationForm
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.success_url)
