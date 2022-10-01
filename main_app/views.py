ff32c767c6cc1e99c1df1e65cf90edc13fbf773d
jeremiahtaylor2021 <jeremiahtaylor2021@gmail.com>
2022-10-01 15:53:31 -0700

    wip: created signup

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 class Home(TemplateView):
     template_name = 'main_app/home.html'
+
+class Signup(CreateView):
+    form_class = UserCreationForm
+    success_url = '/'
+    template_name = 'registration/signup.html'
