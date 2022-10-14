40c8b08f1d7b56dfa75e485e7e305a1805da2c62
robobait <kerrykj8@yahoo.com>
2022-10-13 21:41:40 -0700

    wip: delete page img troubleshooting

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,4 @@
+from operator import concat
 from os import environ
 from django.shortcuts import redirect, render
 from django.views.generic import TemplateView, CreateView, DeleteView
@@ -5,6 +6,7 @@ from django.urls import reverse_lazy
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.core import serializers
+from django.template.defaulttags import register
 from .models import Album
 import requests
 
@@ -71,6 +73,29 @@ class MyAlbums(TemplateView):
 class DeleteAlbum(DeleteView):
     model = Album
     success_url = '/my_albums/'
+    
 
+
+
+    def get_context_data(self, **kwargs):
+        
+        id_int = self.request.path.split("/")[-1]
+        id_str = self.request.path.split("/")[-1].split(" by ")[0]
+        
+        context = super().get_context_data(**kwargs)
+        data = serializers.serialize("python", Album.objects.all())
+
+
+        for obj in data:
+            if obj["pk"] == id_int:
+                context['album_cover'] = obj["fields"]["photo_url"]
+            elif obj["fields"]["title"] == id_str:
+                context['album_cover'] = obj["fields"]["photo_url"]
+        
+        print(context)
+        return context
+    
+
+    
 
 
