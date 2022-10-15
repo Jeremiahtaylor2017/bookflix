3be7870c0632ea0b62cb23a27cc6a37ed0e3e7a0
nathangauth88 <nathangauth@gmail.com>
2022-10-14 22:30:16 -0700

    wip: updates

--- main_app/views.py
+++ main_app/views.py
@@ -1,16 +1,23 @@
+from operator import concat
 from os import environ
-from datetime import datetime
 
-from django.contrib import messages
 from django.shortcuts import redirect, render
 from django.views.generic import TemplateView, CreateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth import login
+
+from django.contrib.auth.forms import UserCreationForm
+
+
 from django.core import serializers
+from .models import Album
 
-from .models import Album, Profile
+import requests
 
+from django.core import serializers
+from .models import Album
 from .forms import CreateUserForm, AlbumSearch
+
 
 import environ
 env = environ.Env()
@@ -21,54 +28,16 @@ environ.Env.read_env()
 class Home(TemplateView):
     template_name = 'main_app/home.html'
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request):
         data = {}
-        form = AlbumSearch(request.GET)
-        profile = Profile.objects.get(user=request.user)
-
-        if form.is_valid():
-            data = form.get_album()
-            if ('error' in data):
-                messages.error(request, 'No album found')
-                return render(request, self.template_name, {'form': form})
-
+        if 'artist' and 'album' in request.GET:
+            form = AlbumSearch(request.GET)
+            if form.is_valid():
+                data = form.get_album()
         else:
             form = AlbumSearch()
 
-        new_album = Album()
-
-        for key, value in data.items():
-            for k, v in value.items():
-                if k == 'name':
-                    new_album.title = v
-                if k == 'artist':
-                    new_album.artist = v
-                if k == 'image':
-                    for index, item in enumerate(v):
-                        if index == 2:
-                            new_album.photo_url = item['#text']
-                if k == 'wiki':
-                    for ke, val in v.items():
-                        if ke == 'published':
-                            new_album.publish_date = val
-                        if ke == 'summary':
-                            new_album.summary = val
-                if k == 'tags':
-                    for ke, val in v.items():
-                        for index, item in enumerate(val):
-                            if index == 0:
-                                print(item['name'])
-                                new_album.genre = item['name']
-                if k == 'tracks':
-                    new_album.track_list = []
-                    for ke, val in v.items():
-                        for index, name in enumerate(val):
-                            new_album.track_list.append(name['name'])
-
-        new_album.save()
-        profile.albums.add(new_album)
         return render(request, self.template_name, {'form': form, 'data': data})
-
 
 
 class Signup(CreateView):
@@ -86,6 +55,7 @@ class ShowAlbumDetails(TemplateView):
     template_name = 'main_app/details.html'
 
 
+
 class Index(TemplateView):
     template_name = 'main_app/index.html'
 
@@ -100,6 +70,7 @@ class MyAlbums(TemplateView):
     def get(self, request):
         data = serializers.serialize("python", Album.objects.filter(profile=request.user.profile))
         return render(request, self.template_name, {'data': data})
+
 
 
 class DeleteAlbum(DeleteView):
@@ -119,3 +90,4 @@ class DeleteAlbum(DeleteView):
                 
         return context
     
+
