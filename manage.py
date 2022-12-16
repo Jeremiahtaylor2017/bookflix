29c023ce9a4806da237e664989c8302b036ee153
jeremiahtaylor2021 <jeremiahtaylor2021@gmail.com>
2022-12-16 00:13:35 -0800

    expose port for railway

--- manage.py
+++ manage.py
@@ -2,6 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 
 def main():
@@ -9,6 +12,8 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookflix.settings')
     try:
         from django.core.management import execute_from_command_line
+        from django.core.management.commands.runserver import Command as runserver
+        runserver.default_port = env('PORT')
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
